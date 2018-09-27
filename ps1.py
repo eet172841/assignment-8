92cf11f06e800adcfbb688221241dfb15d316520
Prateek <Prateek Arora>
2018-09-27 15:17:06 +0530

    ps2 done

--- ps1.py
+++ ps1.py
@@ -6,11 +6,37 @@ for i in range(int(row)):
     str = input()
     s.append(str)
 
-#print (s)
+count = 0
+num = 1
+flag = 0
 
-cross = []
-len = []
+maxlist = []
 
 for i in range(int(row)):
     for j in range(int(col)):
-        if i
+        flag = 0
+        if s[i][j] == 'S':
+            flag = 1
+            num = 1
+            count = 0
+            while i - num >= 0 and i + num < int(row) and j - num >= 0 and j + num < int(col) and s[i-num][j] == 'S' and s[i][j-num] == 'S' and s[i+num][j] == 'S' and s[i][j+num] == 'S':
+                    count += 1
+                    num += 1
+        if flag == 1:
+            maxlist.append(count*4 + 1)
+
+# if flag == 1:
+#     maxlist.append(count*4 + 1)
+
+max1 = 0
+max2 = 0
+
+for i in range(len(maxlist)):
+    if maxlist[i] >= max1:
+        max2 = max1
+        max1 = maxlist[i]
+
+print (max1, max2)
+
+
+#print (s)
